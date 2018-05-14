f3824de5fe75d1691b1aaefe579868c4801f9dec
1064xp <zyf0102@foxmail.com>
2018-05-15 00:23:45 +0800

    修改輸出2

--- src/testgit.java
+++ src/testgit.java
@@ -1,6 +1,6 @@
 public class testgit {
     public static void main(String[] args) {
         System.out.println("sad");
-        System.out.println("asda");
+        System.out.println("asdweqeqa");
     }
 }
