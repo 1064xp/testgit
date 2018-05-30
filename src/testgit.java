151c28e302c19dc6abb3ba06ac1890cf82106937
1064xp <zyf0102@foxmail.com>
2018-05-30 23:37:40 +0800

    测试一下

--- src/testgit.java
+++ src/testgit.java
@@ -1,6 +1,6 @@
 public class testgit {
     public static void main(String[] args) {
-        System.out.println("sad");
-        System.out.println("asd");
+        System.out.println("sad1");
+        System.out.println("asd2");
     }
 }
