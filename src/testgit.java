3a15c65a9480c221202b7e607bf0bdd4309b603d
1064xp <zyf0102@foxmail.com>
2018-05-30 23:56:48 +0800

    Update testgit.java

--- src/testgit.java
+++ src/testgit.java
@@ -1,6 +1,6 @@
 public class testgit {
     public static void main(String[] args) {
-        System.out.println("sad6");
+        System.out.println("sad7");
         System.out.println("asd2");
     }
 }
